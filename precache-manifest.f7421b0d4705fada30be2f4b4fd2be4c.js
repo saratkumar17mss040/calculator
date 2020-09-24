e9ae1f36c5c7b22c4944e2f3e21b4f4bff77d06e
sarath <sarathdon9245@gmail.com>
2020-09-24 22:10:54 +0530

    Updates

---
+++ precache-manifest.f7421b0d4705fada30be2f4b4fd2be4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00a5567b119e5b495210470db1c9441",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "da60e3c278be920b0ec3",
+    "url": "/calculator/static/css/main.db03f093.chunk.css"
+  },
+  {
+    "revision": "1a157abf79826ae28861",
+    "url": "/calculator/static/js/2.47e808ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.47e808ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da60e3c278be920b0ec3",
+    "url": "/calculator/static/js/main.b52e8bde.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
